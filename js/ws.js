5d1480e64de2ad50e5d45853b1c96fc7596afb38
Thomas Dy <thatsmydoing@gmail.com>
2013-09-29 00:28:34 +0800

    Combine geocoder and OTP js files

--- js/ws.js
+++ js/ws.js
@@ -1,3 +1,41 @@
+var geocoder = function() {
+  var scriptDeferred = Q.defer();
+  scriptDeferred.resolve(new google.maps.Geocoder());
+
+  function callGeocode(options) {
+    var requestDeferred = Q.defer();
+    scriptDeferred.promise.then(function(geocoder) {
+      geocoder.geocode(options, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          requestDeferred.resolve(results);
+        }
+        else {
+          requestDeferred.reject(results);
+        }
+      });
+    });
+    return requestDeferred.promise;
+  }
+
+  return {
+    fromName: Q.fbind(function(query, bounds) {
+      return callGeocode({
+        address: query,
+        region: 'ph',
+        bounds: new google.maps.LatLngBounds(
+          new google.maps.LatLng(bounds.getWest(), bounds.getSouth()),
+          new google.maps.LatLng(bounds.getEast(), bounds.getNorth())
+        )
+      });
+    }),
+    fromLatLng: Q.fbind(function(latlng) {
+      return callGeocode({
+        latLng: new google.maps.LatLng(latlng.lat, latlng.lng)
+      });
+    })
+  }
+}();
+
 var otp = function() {
   var API = 'http://maps.pleasantprogrammer.com/opentripplanner-api-webapp/ws'
 
